d72a7a2d1dd52409e8c58a4266c6823c847b8d4f
andreonofre <alordandre@gmail.com>
2023-08-23 21:44:36 -0300

    iniciei o form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
-
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     Teste
+    <div className="container">
+      <div>
+        <h2>Calculadora IMC</h2>
+        <form className="inputUser">
+          <label htmlFor="altura">Digite seu Peso</label>
+          <input type="text" id="altura" />
+          <label htmlFor="peso">Peso</label>
+          <input type="text" id="peso" />
+        </form>
+      </div>
     </div>
   );
 }
